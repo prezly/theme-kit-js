91fded969006287ecf108fb4744b81fba723ea07
Oleg Semyonov <oleg@prezly.com>
2022-02-01 10:42:03 +0300

    Improve type safety a little for `processRequest`

--- src/adapter-nextjs/processRequest.ts
+++ src/adapter-nextjs/processRequest.ts
@@ -5,6 +5,19 @@ import { getRedirectToCanonicalLocale, LocaleObject } from '../intl';
 import { NewsroomContextProps } from '../newsroom-context';
 import { ServerSidePageProps } from '../types';
 
+// TODO: Try to make the typings even more robust
+function extractServerSideProps<Props>(props: Props & ServerSidePageProps): {
+    pageProps: Props;
+    serverSideProps: ServerSidePageProps;
+} {
+    const { localeResolved, ...pageProps } = props;
+
+    return {
+        pageProps: pageProps as unknown as Props,
+        serverSideProps: { localeResolved },
+    };
+}
+
 export function processRequest<Props extends NewsroomContextProps>(
     context: GetServerSidePropsContext,
     props: Props & ServerSidePageProps,
@@ -12,14 +25,14 @@ export function processRequest<Props extends NewsroomContextProps>(
 ): GetServerSidePropsResult<Props> {
     const { locale: nextLocale, query } = context;
 
-    const { localeResolved, ...pageProps } = props;
+    const { pageProps, serverSideProps } = extractServerSideProps(props);
 
-    if (!localeResolved) {
+    if (!serverSideProps.localeResolved) {
         return { notFound: true };
     }
 
     if (canonicalUrl) {
-        const { languages, localeCode } = props;
+        const { languages, localeCode } = pageProps;
         const currentLocale = LocaleObject.fromAnyCode(localeCode);
         const shortestLocaleCode = getShortestLocaleCode(languages, currentLocale);
 
@@ -35,6 +48,6 @@ export function processRequest<Props extends NewsroomContextProps>(
     }
 
     return {
-        props: pageProps as unknown as Props,
+        props: pageProps,
     };
 }
