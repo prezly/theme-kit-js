1777d1c17edac2966275c5b103e2f822ba1c4151
Lukas Kudlik <kudlajzzz@gmail.com>
2022-02-02 17:13:56 +0100

    Pass story via options

--- src/adapter-nextjs/getNewsroomServerSideProps.ts
+++ src/adapter-nextjs/getNewsroomServerSideProps.ts
@@ -1,9 +1,11 @@
+import { Story } from '@prezly/sdk';
 import type { GetServerSidePropsContext } from 'next';
 
 import { getPrezlyApi } from '../data-fetching';
 
 interface Options {
     loadHomepageContacts?: boolean;
+    story?: Story;
 }
 
 export async function getNewsroomServerSideProps(
@@ -13,7 +15,7 @@ export async function getNewsroomServerSideProps(
     const { req: request, locale } = context;
 
     const api = getPrezlyApi(request);
-    const serverSideProps = await api.getNewsroomServerSideProps(request, locale);
+    const serverSideProps = await api.getNewsroomServerSideProps(request, locale, options.story);
 
     if (options.loadHomepageContacts) {
         serverSideProps.newsroomContextProps.contacts = await api.getNewsroomContacts();
