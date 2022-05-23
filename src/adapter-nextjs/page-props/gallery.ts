3ba2460c73cf4da859c140207765c19b2e568c4a
Oleg Semyonov <oleg@prezly.com>
2022-05-23 14:06:57 +0400

    Add media pages props fetching helpers

---
+++ src/adapter-nextjs/page-props/gallery.ts
@@ -0,0 +1,64 @@
+import type { NewsroomGallery } from '@prezly/sdk';
+import type { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+
+import { DEFAULT_GALLERY_PAGE_SIZE } from '../../data-fetching';
+import type { PaginationProps } from '../../infinite-loading/types';
+import { getNewsroomServerSideProps } from '../getNewsroomServerSideProps';
+import { processRequest } from '../processRequest';
+
+import type { PropsFunction } from './lib/types';
+
+export interface GalleryPageProps {
+    galleries: NewsroomGallery[];
+    pagination: PaginationProps;
+}
+
+interface Options {
+    pageSize?: number;
+}
+
+export function getGalleryPageServerSideProps<CustomProps extends Record<string, any>>(
+    customProps: CustomProps | PropsFunction<CustomProps>,
+    options?: Options,
+) {
+    const { pageSize = DEFAULT_GALLERY_PAGE_SIZE } = options || {};
+
+    return async function getServerSideProps(
+        context: GetServerSidePropsContext,
+    ): Promise<GetServerSidePropsResult<GalleryPageProps & CustomProps>> {
+        const { api, serverSideProps } = await getNewsroomServerSideProps(context);
+        const { query } = context;
+
+        const page = query.page && typeof query.page === 'string' ? Number(query.page) : undefined;
+        const { galleries, pagination } = await api.getGalleries({ page, pageSize });
+
+        // If there's only one gallery, redirect to it immediately
+        if (galleries.length === 1) {
+            const { uuid } = galleries[0];
+
+            return {
+                redirect: {
+                    destination: `/media/album/${uuid}`,
+                    permanent: false,
+                },
+            };
+        }
+
+        return processRequest(
+            context,
+            {
+                ...serverSideProps,
+                galleries,
+                pagination: {
+                    itemsTotal: pagination.matched_records_number,
+                    currentPage: page ?? 1,
+                    pageSize,
+                },
+                ...(typeof customProps === 'function'
+                    ? await (customProps as PropsFunction<CustomProps>)(context, serverSideProps)
+                    : customProps),
+            },
+            '/media',
+        );
+    };
+}
--- src/adapter-nextjs/page-props/index.ts
+++ src/adapter-nextjs/page-props/index.ts
@@ -1,4 +1,6 @@
 export { getCategoryPageServerSideProps, type CategoryPageProps } from './category';
+export { getGalleryPageServerSideProps, type GalleryPageProps } from './gallery';
+export { getGalleryAlbumPageServerSideProps, type GalleryAlbumPageProps } from './galleryAlbum';
 export { getHomepageServerSideProps, type HomePageProps } from './home';
 export { getStoryPageServerSideProps } from './story';
 export { getStoryPreviewPageServerSideProps } from './storyPreview';
