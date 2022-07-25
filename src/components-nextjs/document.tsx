2abeae0c0765a52f1014c1959c09e46fb75db584
Oleg Semyonov <oleg@prezly.com>
2022-07-25 13:31:36 +0400

    Update locale code in _document to fail silently when no newsroomContextProps exists

--- src/components-nextjs/document.tsx
+++ src/components-nextjs/document.tsx
@@ -4,13 +4,21 @@ import { DEFAULT_LOCALE, getLocaleDirection, LocaleObject } from '../intl';
 import type { PageProps } from '../types';
 
 export class PrezlyThemeDocument extends Document {
-    render() {
+    getLocaleCode() {
         const {
-            newsroomContextProps: { localeCode = DEFAULT_LOCALE },
+            newsroomContextProps,
             // eslint-disable-next-line no-underscore-dangle
         } = this.props.__NEXT_DATA__.props.pageProps as PageProps;
 
-        const locale = LocaleObject.fromAnyCode(localeCode);
+        // In some cases (like server errors) the newsroom context props will not be loaded.
+        // We need to fall back to default locale to keep the app working.
+        const { localeCode } = newsroomContextProps ?? { localeCode: DEFAULT_LOCALE };
+
+        return localeCode;
+    }
+
+    render() {
+        const locale = LocaleObject.fromAnyCode(this.getLocaleCode());
         // TODO: The direction can be pulled from the Language object
         const direction = getLocaleDirection(locale);
 
