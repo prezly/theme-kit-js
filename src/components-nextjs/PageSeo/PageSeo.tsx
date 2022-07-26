3707bef436f1bcfa97cf66509824616735b319a7
Oleg Semyonov <oleg@prezly.com>
2022-07-26 14:20:09 +0400

    Fix incorrect condition for `noindex` meta on PageSeo

--- src/components-nextjs/PageSeo/PageSeo.tsx
+++ src/components-nextjs/PageSeo/PageSeo.tsx
@@ -95,8 +95,8 @@ export function PageSeo({
             title={pageTitle}
             description={pageDescription}
             canonical={canonicalUrl}
-            noindex={newsroom.is_indexable}
-            nofollow={newsroom.is_indexable}
+            noindex={!newsroom.is_indexable}
+            nofollow={!newsroom.is_indexable}
             openGraph={{
                 url: canonicalUrl,
                 title: pageTitle,
