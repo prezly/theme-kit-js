780f31d4f4618af88c35ad75961aab0aabcbf938
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2022-08-17 14:36:52 +0430

    fix: apply review suggestions

--- src/utils/getAssetsUrl.test.ts
+++ src/utils/getAssetsUrl.test.ts
@@ -1,7 +1,8 @@
+import type { UploadedImage } from '@prezly/sdk';
 import { ASSETS_URL } from './constants';
 import { getAssetsUrl } from './getAssetsUrl';
 
-const uploadCareImage = {
+const uploadcareImage: UploadedImage = {
     version: 2,
     uuid: '43258238-9e71-49fa-a35e-dca5baf16a3b',
     filename: 'prezly-photographe.png',
@@ -10,27 +11,25 @@ const uploadCareImage = {
     original_width: 1162,
     original_height: 369,
     effects: ['/crop/369x369/0,0/', '/preview/'],
-    isImage: true,
-    download_url:
-        'https://cdn.uc.assets.prezly.com/43258238-9e71-49fa-a35e-dca5baf16a3b/-/crop/369x369/0,0/-/preview/-/inline/no/prezly-photographe.png',
-    cdnUrl: 'https://cdn.uc.assets.prezly.com/43258238-9e71-49fa-a35e-dca5baf16a3b/-/crop/369x369/0,0/-/preview/',
+    // isImage: true,
+    // download_url:
+    //     'https://cdn.uc.assets.prezly.com/43258238-9e71-49fa-a35e-dca5baf16a3b/-/crop/369x369/0,0/-/preview/-/inline/no/prezly-photographe.png',
+    // cdnUrl: 'https://cdn.uc.assets.prezly.com/43258238-9e71-49fa-a35e-dca5baf16a3b/-/crop/369x369/0,0/-/preview/',
 };
 
 describe('getAssetsUrl', () => {
     it('generates correct asset URL without effects', () => {
-        const image = { ...uploadCareImage, effects: [] };
+        const image = { ...uploadcareImage, effects: [] };
         expect(getAssetsUrl(image)).toBe(`${ASSETS_URL}/${image.uuid}/`);
     });
 
     it('generates correct asset URL with effects', () => {
-        const image = { ...uploadCareImage };
-        expect(getAssetsUrl(image)).toBe(
-            `${ASSETS_URL}/${image.uuid}/-${image.effects[0]}-${image.effects[1]}`,
+        expect(getAssetsUrl(uploadcareImage)).toBe(
+            `${ASSETS_URL}/${uploadcareImage.uuid}/-${uploadcareImage.effects[0]}-${uploadcareImage.effects[1]}`,
         );
     });
 
     it('generates correct asset URL with uuid passed to it', () => {
-        const image = { ...uploadCareImage };
-        expect(getAssetsUrl(image.uuid)).toBe(`${ASSETS_URL}/${image.uuid}/`);
+        expect(getAssetsUrl(uploadcareImage.uuid)).toBe(`${ASSETS_URL}/${uploadcareImage.uuid}/`);
     });
 });
--- src/utils/getAssetsUrl.ts
+++ src/utils/getAssetsUrl.ts
@@ -3,7 +3,7 @@ import type { UploadedImage } from '@prezly/sdk';
 import { ASSETS_URL } from './constants';
 
 /**
- * @param image The `UploadedImage` object or uuid of the asset hosted on UploadCare
+ * @param image The `UploadedImage` object or uuid of the asset hosted on Uploadcare
  * @returns Full asset URL that can be used as a source for <img>, <video> and <a> tags.
  */
 export function getAssetsUrl(image: UploadedImage | string) {
