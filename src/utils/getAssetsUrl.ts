6cfc685551bd2b0688a4e71d68219581da94e4cf
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2022-08-17 11:41:27 +0430

    fix: apply effects in `getAssetsUrl` fn

--- src/utils/getAssetsUrl.ts
+++ src/utils/getAssetsUrl.ts
@@ -1,9 +1,16 @@
+import type { UploadedImage } from '@prezly/sdk';
+
 import { ASSETS_URL } from './constants';
 
 /**
- * @param uuid UUID of the assets hosted on Uploadcare
+ * @param image `UploadedImage` object of the asset hosted on UploadCare
  * @returns Full asset URL that can be used as a source for <img>, <video> and <a> tags.
  */
-export function getAssetsUrl(uuid: string) {
-    return `${ASSETS_URL}/${uuid}/`;
+export function getAssetsUrl(image: UploadedImage) {
+    const assetUrl = `${ASSETS_URL}/${image.uuid}/`;
+    if (image.effects.length > 0) {
+        const img = `${assetUrl}-${image.effects.join('-')}`;
+        return img;
+    }
+    return assetUrl;
 }
--- src/utils/getNewsroomLogoUrl.ts
+++ src/utils/getNewsroomLogoUrl.ts
@@ -4,7 +4,7 @@ import { getAssetsUrl } from './getAssetsUrl';
 
 export function getNewsroomLogoUrl(newsroom: Newsroom): string {
     if (newsroom.newsroom_logo) {
-        return getAssetsUrl(newsroom.newsroom_logo.uuid);
+        return getAssetsUrl(newsroom.newsroom_logo);
     }
 
     return '';
@@ -13,7 +13,7 @@ export function getNewsroomLogoUrl(newsroom: Newsroom): string {
 export function getNewsroomFaviconUrl(newsroom: Newsroom, previewSize = 400): string {
     const image = newsroom.icon || newsroom.square_logo;
     if (image) {
-        return `${getAssetsUrl(image.uuid)}-/preview/${previewSize}x${previewSize}/-/quality/best/`;
+        return `${getAssetsUrl(image)}-/preview/${previewSize}x${previewSize}/-/quality/best/`;
     }
 
     return '';
