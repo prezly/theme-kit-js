41ae9fac097e9d94b986265fa8e90daed16f17e7
Mohammad Ali Shahbazi <67554982+MohammadxAli@users.noreply.github.com>
2022-08-23 12:09:06 +0430

    fix: apply effects on newsroom logo and favicon (#98)
    * chore: added `@prezly/uploadcare` package
    
    * feat: use `UploadcareImage` class to generate `cdnUrl`

--- src/utils/getAssetsUrl.test.ts
+++ src/utils/getAssetsUrl.test.ts
@@ -2,9 +2,8 @@ import { ASSETS_URL } from './constants';
 import { getAssetsUrl } from './getAssetsUrl';
 
 describe('getAssetsUrl', () => {
-    it('generates correct asset URL', () => {
-        const uuid = 'test-uuid';
-
+    it('generates correct asset URL with uuid passed to it', () => {
+        const uuid = 'foo-bar';
         expect(getAssetsUrl(uuid)).toBe(`${ASSETS_URL}/${uuid}/`);
     });
 });
