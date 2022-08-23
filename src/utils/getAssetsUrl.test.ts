209d0dc9cc1ac9f49d71b201b82858511a226de8
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2022-08-23 10:09:48 +0430

    refactor: revert changes on `getAssetsUrl` as its not needed

--- src/utils/getAssetsUrl.test.ts
+++ src/utils/getAssetsUrl.test.ts
@@ -1,31 +1,9 @@
-import type { UploadedImage } from '@prezly/sdk';
 import { ASSETS_URL } from './constants';
 import { getAssetsUrl } from './getAssetsUrl';
 
-const uploadcareImage: UploadedImage = {
-    version: 2,
-    uuid: '43258238-9e71-49fa-a35e-dca5baf16a3b',
-    filename: 'prezly-photographe.png',
-    mime_type: 'image/png',
-    size: 74234,
-    original_width: 1162,
-    original_height: 369,
-    effects: ['/crop/369x369/0,0/', '/preview/'],
-};
-
 describe('getAssetsUrl', () => {
-    it('generates correct asset URL without effects', () => {
-        const image = { ...uploadcareImage, effects: [] };
-        expect(getAssetsUrl(image)).toBe(`${ASSETS_URL}/${image.uuid}/`);
-    });
-
-    it('generates correct asset URL with effects', () => {
-        expect(getAssetsUrl(uploadcareImage)).toBe(
-            `${ASSETS_URL}/${uploadcareImage.uuid}/-${uploadcareImage.effects[0]}-${uploadcareImage.effects[1]}`,
-        );
-    });
-
     it('generates correct asset URL with uuid passed to it', () => {
-        expect(getAssetsUrl(uploadcareImage.uuid)).toBe(`${ASSETS_URL}/${uploadcareImage.uuid}/`);
+        const uuid = 'foo-bar';
+        expect(getAssetsUrl(uuid)).toBe(`${ASSETS_URL}/${uuid}/`);
     });
 });
--- src/utils/getAssetsUrl.ts
+++ src/utils/getAssetsUrl.ts
@@ -1,17 +1,9 @@
-import type { UploadedImage } from '@prezly/sdk';
-
 import { ASSETS_URL } from './constants';
 
 /**
- * @param image The `UploadedImage` object or uuid of the asset hosted on Uploadcare
+ * @param uuid UUID of the assets hosted on Uploadcare
  * @returns Full asset URL that can be used as a source for <img>, <video> and <a> tags.
  */
-export function getAssetsUrl(image: UploadedImage | string) {
-    if (typeof image === 'string') {
-        return `${ASSETS_URL}/${image}/`;
-    }
-    if (image.effects.length > 0) {
-        return `${ASSETS_URL}/${image.uuid}/-${image.effects.join('-')}`;
-    }
-    return `${ASSETS_URL}/${image.uuid}/`;
+export function getAssetsUrl(uuid: string) {
+    return `${ASSETS_URL}/${uuid}/`;
 }
