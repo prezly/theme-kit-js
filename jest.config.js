fa0e55f75bb6d1cd339346da060da6d18b1fda12
aspirisen <aspirisen@gmail.com>
2022-12-07 21:27:18 +0400

    Use esm modules in tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest').InitialOptionsTsJest} */
 module.exports = {
-    preset: 'ts-jest',
+    preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
     collectCoverage: true,
     globals: {
         'ts-jest': {
             tsconfig: 'tsconfig.test.json',
+            useESM: true,
         },
     },
 };
--- src/__mocks__/languages.ts
+++ src/__mocks__/languages.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import { type NewsroomLanguageSettings, Culture } from '@prezly/sdk';
 
 /**
