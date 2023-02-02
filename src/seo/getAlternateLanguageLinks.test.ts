e70f930e2fb16070d654fc76739ab49497cf4cee
aspirisen <aspirisen@gmail.com>
2023-02-03 01:15:48 +0400

    Add x-default handling

--- src/seo/getAlternateLanguageLinks.test.ts
+++ src/seo/getAlternateLanguageLinks.test.ts
@@ -1,21 +1,137 @@
+import { NewsroomLanguageSettings } from '@prezly/sdk';
+import { LocaleCode, LocaleObject } from '../intl';
 import { getAlternateLanguageLinks } from './getAlternateLanguageLinks';
 
+function lang(code: LocaleCode, isDefault = false) {
+    return { code, is_default: isDefault } as NewsroomLanguageSettings;
+}
+
+function getTranslationUrl(locale: LocaleObject) {
+    return `translationUrl/${locale.toHyphenCode()}`;
+}
+
+function createHref(locale: LocaleObject, translationUrl: string) {
+    return `http://localhost:3000/${locale.toNeutralLanguageCode()}/${translationUrl}`;
+}
+
 describe('getAlternateLanguageLinks', () => {
-    it('test', () => {
-        const res = getAlternateLanguageLinks(
+    it('should handle one en-GB non-default language', () => {
+        const links = getAlternateLanguageLinks([lang('en-GB')], getTranslationUrl, createHref);
+
+        [
+            { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en-GB' },
+            { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en' },
+            { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'x-default' },
+        ];
+
+        expect(links).toMatchObject(
             [
-                { code: 'en-US' },
-                { code: 'en-GB' },
-                { code: 'fr-FR' },
-                { code: 'fr', is_default: true },
-            ] as any,
-            (locale) => `translationUrl/${locale.toHyphenCode()}`,
-            (locale, translationUrl) =>
-                `http://localhost:3000/${locale.toNeutralLanguageCode()}/${translationUrl}`,
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en-GB' },
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en' },
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'x-default' },
+            ].sort((a, b) => a.hrefLang.localeCompare(b.hrefLang)),
+        );
+    });
+
+    it('should handle one en-GB default language', () => {
+        const links = getAlternateLanguageLinks(
+            [lang('en-GB', true)],
+            getTranslationUrl,
+            createHref,
         );
 
-        console.log(res);
+        expect(links).toMatchObject(
+            [
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en-GB' },
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en' },
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'x-default' },
+            ].sort((a, b) => a.hrefLang.localeCompare(b.hrefLang)),
+        );
+    });
 
-        expect(true).toBe(true);
+    it('should handle two full locales', () => {
+        const links = getAlternateLanguageLinks(
+            [lang('en-GB', true), lang('fr-FR')],
+            getTranslationUrl,
+            createHref,
+        );
+
+        expect(links).toMatchObject(
+            [
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en-GB' },
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en' },
+                { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'fr-FR' },
+                { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'fr' },
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'x-default' },
+            ].sort((a, b) => a.hrefLang.localeCompare(b.hrefLang)),
+        );
+    });
+
+    it('should use en-GB locale as x-default even if other locale is set as default', () => {
+        const links = getAlternateLanguageLinks(
+            [lang('en-GB'), lang('fr-FR', true)],
+            getTranslationUrl,
+            createHref,
+        );
+
+        expect(links).toMatchObject(
+            [
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en-GB' },
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'en' },
+                { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'fr-FR' },
+                { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'fr' },
+                { href: 'http://localhost:3000/en/translationUrl/en-GB', hrefLang: 'x-default' },
+            ].sort((a, b) => a.hrefLang.localeCompare(b.hrefLang)),
+        );
+    });
+
+    it('should use default locale as x-default when there is eng locale', () => {
+        const links = getAlternateLanguageLinks(
+            [lang('ru-RU'), lang('fr-FR', true)],
+            getTranslationUrl,
+            createHref,
+        );
+
+        expect(links).toMatchObject(
+            [
+                { href: 'http://localhost:3000/ru/translationUrl/ru-RU', hrefLang: 'ru-RU' },
+                { href: 'http://localhost:3000/ru/translationUrl/ru-RU', hrefLang: 'ru' },
+                { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'fr-FR' },
+                { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'fr' },
+                { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'x-default' },
+            ].sort((a, b) => a.hrefLang.localeCompare(b.hrefLang)),
+        );
+    });
+
+    it('should use provided global locale', () => {
+        const links = getAlternateLanguageLinks(
+            [lang('en-US'), lang('fr'), lang('fr-FR', true)],
+            getTranslationUrl,
+            createHref,
+        );
+
+        expect(links).toMatchObject(
+            [
+                { href: 'http://localhost:3000/en/translationUrl/en-US', hrefLang: 'en-US' },
+                { href: 'http://localhost:3000/en/translationUrl/en-US', hrefLang: 'en' },
+                { href: 'http://localhost:3000/fr/translationUrl/fr', hrefLang: 'fr' },
+                { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'fr-FR' },
+                { href: 'http://localhost:3000/en/translationUrl/en-US', hrefLang: 'x-default' },
+            ].sort((a, b) => a.hrefLang.localeCompare(b.hrefLang)),
+        );
+    });
+
+    it('should should omit locales if getTranslationUrl is undefined', () => {
+        const links = getAlternateLanguageLinks(
+            [lang('en-US'), lang('fr'), lang('fr-FR', true)],
+            (locale) => (locale.toHyphenCode() === 'en-US' ? undefined : getTranslationUrl(locale)),
+            createHref,
+        );
+
+        expect(links).toMatchObject([
+            { href: 'http://localhost:3000/fr/translationUrl/fr', hrefLang: 'fr' },
+            { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'fr-FR' },
+            { href: 'http://localhost:3000/fr/translationUrl/fr-FR', hrefLang: 'x-default' },
+        ]);
     });
 });
--- src/seo/getAlternateLanguageLinks.ts
+++ src/seo/getAlternateLanguageLinks.ts
@@ -4,61 +4,105 @@ import type { AlternateLanguageLink } from '../components-nextjs/PageSeo';
 import type { LangCode, LocaleCode } from '../intl';
 import { getFallbackLocale, LocaleObject } from '../intl';
 
-export function mapLanguagesToLocales(languages: NewsroomLanguageSettings[]) {
+function mapLanguagesToLocales(languages: NewsroomLanguageSettings[]) {
+    let defaultLocale: LocaleObject | undefined;
     const localesByLanguage = new Map<LangCode, Map<LocaleCode, LocaleObject>>();
 
     languages.forEach((language) => {
         const locale = LocaleObject.fromAnyCode(language.code);
-        const languageCode = locale.toNeutralLanguageCode();
-        let currentLocale = localesByLanguage.get(languageCode);
+        const globalLanguageCode = locale.toNeutralLanguageCode();
+        let currentLocale = localesByLanguage.get(globalLanguageCode);
 
         if (!currentLocale) {
             currentLocale = new Map([[locale.toHyphenCode(), locale]]);
-            localesByLanguage.set(languageCode, currentLocale);
+            localesByLanguage.set(globalLanguageCode, currentLocale);
         }
 
         currentLocale.set(locale.toHyphenCode(), locale);
+
+        if (language.is_default) {
+            defaultLocale = locale;
+        }
     });
 
-    return localesByLanguage;
+    return [localesByLanguage, defaultLocale] as const;
 }
 
 function createAlternateLanguageLink(
     locale: LocaleObject,
-    translationUrl: string,
+    getTranslationUrl: (locale: LocaleObject) => string | undefined,
     createHref: (locale: LocaleObject, translationUrl: string) => string,
 ) {
+    const translationUrl = getTranslationUrl(locale);
+
+    if (!translationUrl) {
+        return undefined;
+    }
+
     return {
         hrefLang: locale.toHyphenCode(),
         href: createHref(locale, translationUrl),
     };
 }
 
+function getXdefaultHrefLang(
+    localeAlternates: LocaleObject[],
+    globalLocalesFallbackLinks: AlternateLanguageLink[],
+    defaultLocale: LocaleObject | undefined,
+    getTranslationUrl: (locale: LocaleObject) => string | undefined,
+    createHref: (locale: LocaleObject, translationUrl: string) => string,
+): AlternateLanguageLink | undefined {
+    const engFallback = 'en';
+
+    const fallbackFromLocales = localeAlternates.find(
+        (alternate) => alternate.isGlobal && alternate.toNeutralLanguageCode() === engFallback,
+    );
+
+    if (fallbackFromLocales) {
+        const link = createAlternateLanguageLink(
+            fallbackFromLocales,
+            getTranslationUrl,
+            createHref,
+        );
+
+        if (link) {
+            return { ...link, hrefLang: 'x-default' };
+        }
+    }
+
+    const fallbackFromGlobalLocales = globalLocalesFallbackLinks.find(
+        (alternate) => alternate.hrefLang === engFallback,
+    );
+
+    if (fallbackFromGlobalLocales) {
+        return { ...fallbackFromGlobalLocales, hrefLang: 'x-default' };
+    }
+
+    if (defaultLocale) {
+        const link = createAlternateLanguageLink(defaultLocale, getTranslationUrl, createHref);
+
+        if (link) {
+            return { ...link, hrefLang: 'x-default' };
+        }
+    }
+
+    return undefined;
+}
+
 export function getAlternateLanguageLinks(
     languages: NewsroomLanguageSettings[],
     getTranslationUrl: (locale: LocaleObject) => string | undefined,
     createHref: (locale: LocaleObject, translationUrl: string) => string,
-) {
-    const languageAlternates: AlternateLanguageLink[] = [];
-    const localesMap = mapLanguagesToLocales(languages);
+): AlternateLanguageLink[] {
+    const globalLocalesFallbackLinks: AlternateLanguageLink[] = [];
+    const localeAlternates: LocaleObject[] = [];
+    const [localesMap, defaultLocale] = mapLanguagesToLocales(languages);
 
-    localesMap.forEach((locales, localeCode) => {
+    localesMap.forEach((locales, globalLocaleCode) => {
         let hasGlobalLocale = false;
 
         locales.forEach((locale) => {
-            const translationUrl = getTranslationUrl(locale);
-
-            if (!translationUrl) {
-                return;
-            }
-
-            const alternateLanguageLink = createAlternateLanguageLink(
-                locale,
-                translationUrl,
-                createHref,
-            );
-
-            languageAlternates.push(alternateLanguageLink);
+            localeAlternates.push(locale);
 
             if (!hasGlobalLocale && locale.isGlobal) {
                 hasGlobalLocale = true;
@@ -67,48 +111,43 @@ export function getAlternateLanguageLinks(
 
         if (!hasGlobalLocale) {
             const localesArray = Array.from(locales.values());
-            const availableLocales = localesArray;
-            const fallback = getFallbackLocale(localeCode, availableLocales) ?? localesArray.at(0);
+            const fallback =
+                getFallbackLocale(globalLocaleCode, localesArray) ?? localesArray.at(0);
 
             if (fallback) {
-                const translationUrl = getTranslationUrl(fallback);
-
-                if (!translationUrl) {
-                    return;
-                }
-
-                const alternateLanguageLink = createAlternateLanguageLink(
-                    LocaleObject.fromAnyCode(localeCode),
-                    translationUrl,
+                const fallbackLink = createAlternateLanguageLink(
+                    fallback,
+                    getTranslationUrl,
                     createHref,
                 );
 
-                languageAlternates.push(alternateLanguageLink);
+                if (fallbackLink) {
+                    globalLocalesFallbackLinks.push({
+                        ...fallbackLink,
+                        hrefLang: globalLocaleCode,
+                    });
+                }
             }
         }
     });
 
-    const defaultHrefLang =
-        languageAlternates.find((alternate) => alternate.hrefLang === 'en') ??
-        languages.find(
-            (language) =>
-                language.is_default && getTranslationUrl(LocaleObject.fromAnyCode(language.code)),
-        );
+    let languageAlternates: Array<AlternateLanguageLink | undefined> = localeAlternates.map(
+        (locale) => createAlternateLanguageLink(locale, getTranslationUrl, createHref),
+    );
 
-    if (defaultHrefLang) {
-        if ('hrefLang' in defaultHrefLang) {
-            languageAlternates.push({ ...defaultHrefLang, hrefLang: 'x-default' });
-        } else {
-            languageAlternates.push({
-                ...createAlternateLanguageLink(
-                    LocaleObject.fromAnyCode(defaultHrefLang.code),
-                    getTranslationUrl(LocaleObject.fromAnyCode(defaultHrefLang.code))!,
-                    createHref,
-                ),
-                hrefLang: 'x-default',
-            });
-        }
-    }
+    languageAlternates = languageAlternates.concat(globalLocalesFallbackLinks);
 
-    return languageAlternates;
+    const xDefault = getXdefaultHrefLang(
+        localeAlternates,
+        globalLocalesFallbackLinks,
+        defaultLocale,
+        getTranslationUrl,
+        createHref,
+    );
+
+    languageAlternates.push(xDefault);
+
+    return languageAlternates
+        .filter((link): link is Exclude<typeof link, undefined> => Boolean(link))
+        .sort((a, b) => a.hrefLang.localeCompare(b.hrefLang));
 }
