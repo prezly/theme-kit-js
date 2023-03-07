69c9224816591d0507cf517ee1766571dfb32e39
Oleg Semyonov <oleg@prezly.com>
2023-03-07 16:51:04 +0400

    Follow-up

--- src/sentry/server.ts
+++ src/sentry/server.ts
@@ -2,6 +2,9 @@ import type { init } from '@sentry/nextjs';
 
 import { getCommonClientOptions } from './common';
 
-export function getSentryServerOptions(dsn: string, themeName: string): Parameters<typeof init>[0] {
+export function getSentryServerOptions(
+    dsn: string,
+    themeName: string | null,
+): Parameters<typeof init>[0] {
     return getCommonClientOptions(dsn, themeName);
 }
