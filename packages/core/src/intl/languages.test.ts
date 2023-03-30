a4f75eb47bd4c0439d3bb39ba6f23e52ea022c5d
Oleg Semyonov <oleg@prezly.com>
2023-03-30 18:48:22 +0400

    Remove extra tests from core package

--- packages/core/src/intl/languages.test.ts
+++ packages/core/src/intl/languages.test.ts
@@ -8,12 +8,10 @@ import {
     getLanguageByNeutralLocaleCode,
     getLanguageByShortRegionCode,
     getLanguageDisplayName,
-    getLanguageFromNextLocaleIsoCode,
     getLanguageFromStory,
     getShortestLocaleCode,
     getUsedLanguages,
 } from './languages';
-import { DUMMY_DEFAULT_LOCALE } from './locale';
 import { LocaleObject } from './localeObject';
 
 const ALL_LANGUAGES = Object.values(LANGUAGES);
@@ -140,37 +138,6 @@ describe('getLanguageByShortRegionCode', () => {
         expect(getLanguageByShortRegionCode(languages, LocaleObject.fromAnyCode('be'))).toEqual(
             LANGUAGES.fr_BE,
         );
-    });
-});
-
-describe('getLanguageFromNextLocaleIsoCode', () => {
-    it('returns default language when no locale code provided or when dummy locale provided', () => {
-        expect(getLanguageFromNextLocaleIsoCode(ALL_LANGUAGES)).toEqual(LANGUAGES.en);
-        expect(getLanguageFromNextLocaleIsoCode(ALL_LANGUAGES, DUMMY_DEFAULT_LOCALE)).toEqual(
-            LANGUAGES.en,
-        );
-    });
-
-    it('returns language by exact code first', () => {
-        expect(getLanguageFromNextLocaleIsoCode(ALL_LANGUAGES, 'en')).toEqual(LANGUAGES.en);
-    });
-
-    it('returns language by region code second', () => {
-        expect(getLanguageFromNextLocaleIsoCode(ALL_LANGUAGES, 'be')).toEqual(LANGUAGES.nl_BE);
-        expect(getLanguageFromNextLocaleIsoCode(ALL_LANGUAGES, 'nl')).toEqual(LANGUAGES.nl_NL);
-    });
-
-    it('returns language by neutral code thrid', () => {
-        expect(
-            getLanguageFromNextLocaleIsoCode(
-                ALL_LANGUAGES.filter(({ code }) => code !== 'fr'),
-                'fr',
-            ),
-        ).toEqual(LANGUAGES.fr_BE);
-    });
-
-    it('returns undefined when no language found', () => {
-        expect(getLanguageFromNextLocaleIsoCode(ALL_LANGUAGES, 'zh-Hant')).toEqual(undefined);
     });
 });
 
--- packages/core/src/intl/locale.test.ts
+++ packages/core/src/intl/locale.test.ts
@@ -1,12 +1,6 @@
 import { jest } from '@jest/globals';
 
-import {
-    DEFAULT_LOCALE,
-    DUMMY_DEFAULT_LOCALE,
-    getLocaleDirection,
-    getRedirectToCanonicalLocale,
-    getSupportedLocaleIsoCode,
-} from './locale';
+import { DEFAULT_LOCALE, getLocaleDirection, getSupportedLocaleIsoCode } from './locale';
 import { LocaleObject } from './localeObject';
 
 describe('getSupportedLocaleIsoCode', () => {
@@ -35,47 +29,6 @@ describe('getSupportedLocaleIsoCode', () => {
         expect(consoleSpy).toHaveBeenCalledWith(
             'Unsupported locale provided. Please use `LocaleObject.fromAnyCode` to ensure you are using a correct language code',
         );
-    });
-});
-
-describe('getRedirectToCanonicalLocale', () => {
-    it('returns undefined when no locale or dummy locale is provided', () => {
-        expect(getRedirectToCanonicalLocale('en', undefined, '/')).toBe(undefined);
-        expect(getRedirectToCanonicalLocale('en', DUMMY_DEFAULT_LOCALE, '/')).toBe(undefined);
-    });
-
-    it('returns undefined when locale is already canonical', () => {
-        expect(getRedirectToCanonicalLocale('en', 'en', '/')).toBe(undefined);
-        expect(getRedirectToCanonicalLocale('nl_BE', 'nl-be', '/')).toBe(undefined);
-    });
-
-    it('returns a redirect object when requested locale is not canonical', () => {
-        expect(getRedirectToCanonicalLocale(false, 'en-us', '/test')).toEqual({
-            destination: '/test',
-            permanent: false,
-        });
-
-        expect(getRedirectToCanonicalLocale('en', 'en-us', '/test')).toEqual({
-            destination: '/en/test',
-            permanent: false,
-        });
-
-        expect(getRedirectToCanonicalLocale('en', 'en-us', 'test')).toEqual({
-            destination: '/en/test',
-            permanent: false,
-        });
-    });
-
-    it('preserves query parameters', () => {
-        expect(
-            getRedirectToCanonicalLocale('en', 'en-us', '/test', {
-                page: '1',
-                tags: ['story', 'media'],
-            }),
-        ).toEqual({
-            destination: '/en/test?page=1&tags=story&tags=media',
-            permanent: false,
-        });
     });
 });
 
