0df1a5b0a33144c417ea5fad46b43ff2c8920490
Oleg Semyonov <oleg@prezly.com>
2023-04-20 12:18:24 +0400

    Provide reduced /client and /server exports for Next 13 /app directory support

---
+++ packages/nextjs/src/client.ts
@@ -0,0 +1,8 @@
+/**
+ * This is a subset export created for Next 13 /app directory, which doesn't support tree-shaking properly.
+ * See https://github.com/vercel/next.js/issues/12557
+ */
+
+export * from './components-nextjs';
+export * from './infinite-loading';
+export * from './newsroom-context';
