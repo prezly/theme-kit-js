9de3d36dd6596f7ee9cfff54bdb2da052ed0dc4e
Oleg Semyonov <oleg@prezly.com>
2023-05-02 18:35:58 +0400

    Use a more decent Vercel domain name

--- packages/core/src/utils/constants.ts
+++ packages/core/src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const ASSETS_URL = 'https://cdn.uc.assets.prezly.com';
 export const DEFAULT_PAGE_SIZE = 12;
 export const DEFAULT_GALLERY_PAGE_SIZE = 6;
-export const OG_IMAGE_API_URL = 'https://og-image-rho-lovat.vercel.app/api/og';
+export const OG_IMAGE_API_URL = 'https://prezly-og-image.vercel.app/api/og';
