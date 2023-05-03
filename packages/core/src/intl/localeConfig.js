fd64a2a8a4060f8e6529a3e2e132a229180a208e
Oleg Semyonov <oleg@prezly.com>
2023-05-03 11:45:59 +0400

    Remove invalid 4-letter codes from `localeConfig`

--- packages/core/src/intl/localeConfig.js
+++ packages/core/src/intl/localeConfig.js
@@ -16,7 +16,9 @@ const lowercaseLocalPermutations = Array.from(
             .map((code) => code.split('-')[1])
             .filter(Boolean)
             // Remove number-only codes from possible permutations (like `419` for `es-419`)
-            .filter((code) => Number.isNaN(Number(code))),
+            .filter((code) => Number.isNaN(Number(code)))
+            // Remove codes that are too long to be valid standalone codes (like `hant` for `zh-hant`)
+            .filter((code) => code.length > 3),
     ]),
 ).sort();
 
