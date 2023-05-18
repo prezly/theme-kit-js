06a0a4eb6f92238e6643e04afcfdddc6d86a39a1
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2023-05-19 01:25:19 +0330

    Setup dependency omit script

---
+++ .pnpmfile.cjs
@@ -0,0 +1,44 @@
+const OMIT_DEPENDENCIES = [
+    '@types/react',
+    '@types/react-dom',
+    'react',
+    'react-dom',
+    'next',
+    '@playwright/test',
+    '@prezly/sdk',
+];
+
+function readPackage(pkg, context) {
+    if (pkg.name === 'theme-kit') {
+        // do not modify the workspace root package.json
+        return pkg;
+    }
+
+    OMIT_DEPENDENCIES.forEach((dependencyName) => {
+        if (pkg.peerDependencies && pkg.peerDependencies[dependencyName]) {
+            pkg.peerDependencies = omitDependency(pkg.peerDependencies, dependencyName);
+            context.log(`Skipping "${dependencyName}" from "${pkg.name}" peerDependencies.`);
+        }
+        if (pkg.dependencies && pkg.dependencies[dependencyName]) {
+            pkg.dependencies = omitDependency(pkg.dependencies, dependencyName);
+            context.log(`Skipping "${dependencyName}" from "${pkg.name}" dependencies.`);
+        }
+    });
+
+    return pkg;
+}
+
+/**
+ * @param {Record<string,string>} dependencies
+ * @param {string} omit
+ * @returns {Record<string,string>}
+ */
+function omitDependency(dependencies, omit) {
+    return Object.fromEntries(Object.entries(dependencies).filter(([name]) => name !== omit));
+}
+
+module.exports = {
+    hooks: {
+        readPackage,
+    },
+};
