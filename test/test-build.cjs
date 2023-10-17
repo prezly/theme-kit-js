af1ae89ec0737684210644ff5e05f52bc8cf07a6
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-10-17 17:24:21 +0300

    Add explanation comments to the `test-build.*` files

--- test/test-build.cjs
+++ test/test-build.cjs
@@ -1,3 +1,7 @@
+// Make sure that package.json is properly configured
+// and the build output is compatible with CommonJS.
+// By checking it with real Node.js loader.
+
 const assert = require('assert');
 
 const { translations } = require('@prezly/themes-intl-messages');
--- test/test-build.mjs
+++ test/test-build.mjs
@@ -1,3 +1,12 @@
+// Make sure that package.json is properly configured
+// and the build output is compliant with the ES Modules spec.
+//
+// The easiest way to check ESM compatibility is
+// by running it through the Node.js ESM implementation.
+//
+// @see https://nodejs.org/api/esm.html
+// @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+
 import assert from 'assert';
 
 import { translations } from '@prezly/themes-intl-messages';
