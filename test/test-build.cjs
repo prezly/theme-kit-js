254d4fcd644d1e3fc15ed25dda5a0034089c4a1a
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-10-17 14:13:44 +0300

    Extract & improve dist folder artifacts tests

---
+++ test/test-build.cjs
@@ -0,0 +1,5 @@
+const assert = require('assert');
+
+const translations = require('../dist/cjs/');
+
+assert(translations.actions.backToHomePage.id === 'actions.backToHomepage');
---
+++ test/test-build.mjs
@@ -0,0 +1,5 @@
+import assert from 'assert';
+
+import translations from '../dist/esm/index.js';
+
+assert(translations.actions.backToHomePage.id === 'actions.backToHomepage');
