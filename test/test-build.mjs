a5dbebebf1ff56153b958bf61fc865aa65cb4cc0
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-10-17 14:39:29 +0300

    Make dual CJS/ESM build produce spec-compliant output

--- test/test-build.mjs
+++ test/test-build.mjs
@@ -1,5 +1,5 @@
 import assert from 'assert';
 
-import { translations } from '../dist/esm/index.js';
+import { translations } from '../dist/esm/index.mjs';
 
 assert(translations.actions.backToHomePage.id === 'actions.backToHomepage');
