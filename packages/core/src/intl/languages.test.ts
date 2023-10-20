ce103578edb1adfa15aa68a33eaab710a640ed39
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-10-20 11:27:11 +0300

    Add a test for RTL language display name getter
    (just in case)

--- packages/core/src/intl/languages.test.ts
+++ packages/core/src/intl/languages.test.ts
@@ -1,3 +1,7 @@
+import { expect } from '@playwright/test';
+import type { CultureRef } from '@prezly/sdk';
+import { Culture } from '@prezly/sdk';
+
 import { LANGUAGES } from '../__mocks__/languages';
 import { STORY } from '../__mocks__/story';
 
@@ -16,18 +20,35 @@ import {
 const ALL_LANGUAGES = Object.values(LANGUAGES);
 
 describe('getLanguageDisplayName', () => {
-    it('returns shortened name when language is the only culture', () => {
+    it('should return shortened name when language is the only culture', () => {
         const languages = [LANGUAGES.en, LANGUAGES.fr];
 
         expect(getLanguageDisplayName(LANGUAGES.en, languages)).toBe('English');
         expect(getLanguageDisplayName(LANGUAGES.fr, languages)).toBe('Français');
     });
 
-    it('returns full name when language is NOT the only culture', () => {
+    it('should return full name when language is NOT the only culture', () => {
         const languages = [LANGUAGES.nl_BE, LANGUAGES.nl_NL];
 
         expect(getLanguageDisplayName(LANGUAGES.nl_BE, languages)).toBe('Nederlands (België)');
         expect(getLanguageDisplayName(LANGUAGES.nl_NL, languages)).toBe('Nederlands (Nederland)');
+    });
+
+    it('should properly cut region from an RTL language name', () => {
+        const languages = [
+            {
+                locale: {
+                    code: 'ar_AE',
+                    locale: 'ar_AE',
+                    language_code: 'ar',
+                    name: 'Arabic (UAE)',
+                    native_name: 'العربية (الإمارات العربية المتحدة)',
+                    direction: Culture.TextDirection.RTL,
+                } satisfies CultureRef,
+            },
+        ];
+
+        expect(getLanguageDisplayName(languages[0], languages)).toBe('العربية');
     });
 });
 
