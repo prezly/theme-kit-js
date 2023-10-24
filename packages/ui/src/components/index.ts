cf555d4f58d152adedc3277720102ed6b00e78f5
Francis Gyimah <fgyimah883@gmail.com>
2023-10-24 22:30:04 +0000

    [DEV-11850] Added stories for StoryCard

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,4 +2,5 @@ export { Button, ButtonLink } from './Button';
 export { CategoriesList } from './CategoriesList';
 export { CategoryLink } from './CategoryLink';
 export { Hero } from './Hero';
+export { StoryCard } from './StoryCard';
 export { StoryPublicationDate } from './StoryPublicationDate';
