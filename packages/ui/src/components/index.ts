8aafa36289efe969b752a9aee4f043a3a54dd1c9
Francis Gyimah <fgyimah883@gmail.com>
2023-10-25 18:28:18 +0000

    [DEV-11864] Added Input component

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,5 +2,6 @@ export { Button, ButtonLink } from './Button';
 export { CategoriesList } from './CategoriesList';
 export { CategoryLink } from './CategoryLink';
 export { Hero } from './Hero';
+export { Input } from './Input';
 export { StoryCard } from './StoryCard';
 export { StoryPublicationDate } from './StoryPublicationDate';
