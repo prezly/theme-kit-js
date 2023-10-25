2af945ac5162aa018cae85ceedfdda005831f7a1
Francis Gyimah <fgyimah883@gmail.com>
2023-10-25 18:29:38 +0000

    [DEV-11864] Added stories for Input

---
+++ packages/ui/src/components/Input/Input.stories.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable func-style */
+import type { Meta, StoryFn } from '@storybook/react';
+
+import { Input } from '.';
+
+export default {
+    title: 'Components/Input',
+    component: Input,
+    decorators: [
+        (Story) => (
+            <div className="bg-gray-100 p-10">
+                <Story />
+            </div>
+        ),
+    ],
+} as Meta<typeof Input>;
+
+const InputTemplate: StoryFn<typeof Input> = (args) => <Input {...args} />;
+
+export const Default = InputTemplate.bind({});
+Default.args = {
+    placeholder: 'Placeholder',
+};
+
+export const WithError = InputTemplate.bind({});
+WithError.args = {
+    placeholder: 'Placeholder',
+    error: 'Invalid input',
+};
