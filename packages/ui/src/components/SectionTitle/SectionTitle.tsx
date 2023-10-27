b8dd260307edcad873a21ba33de56bedda0e9e59
Francis Gyimah <fgyimah883@gmail.com>
2023-10-27 08:22:46 +0000

    [DEV-11867] Added SectionTitle component

---
+++ packages/ui/src/components/SectionTitle/SectionTitle.tsx
@@ -0,0 +1,16 @@
+import { twMerge } from 'tailwind-merge';
+
+export interface Props {
+    title: string;
+    description?: string;
+    className?: string;
+}
+
+export function SectionTitle({ title, description, className }: Props) {
+    return (
+        <div className={twMerge('container px-6 sm:px-12 py-6 flex flex-col gap-3', className)}>
+            <h1 className="title-medium">{title}</h1>
+            {description && <p className="subtitle-medium">{description}</p>}
+        </div>
+    );
+}
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -7,3 +7,4 @@ export { Hero } from './Hero';
 export { Input } from './Input';
 export { StoryCard } from './StoryCard';
 export { StoryPublicationDate } from './StoryPublicationDate';
+export { SectionTitle } from './SectionTitle';
