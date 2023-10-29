12c4ec06bf45f7ae016fc4f9039b7a845d73142d
Francis Gyimah <fgyimah883@gmail.com>
2023-10-29 19:07:12 +0000

    [DEV-11867] Replaced microbundle with vite to fix build errors

--- packages/ui/src/components/SocialMedia/SocialMedia.tsx
+++ packages/ui/src/components/SocialMedia/SocialMedia.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { ArrowUpIcon, LinkIcon } from '@heroicons/react/24/outline';
 import type { NewsroomCompanyInformation } from '@prezly/sdk';
 import { type MouseEvent as ReactMouseEvent, useEffect, useState } from 'react';
