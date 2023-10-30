4f2ea43efd4576622f1ea7a5ef1f11c2805f7899
Francis Gyimah <fgyimah883@gmail.com>
2023-10-30 15:46:56 +0000

    [DEV-11867] Updated build to support only es and cjs formats

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
             name: 'theme-kit-ui',
-            formats: ['es', 'umd'],
+            formats: ['es', 'cjs'],
             fileName: (format) => `index.${format}.js`,
         },
         rollupOptions: {
