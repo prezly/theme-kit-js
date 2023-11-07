53d446add7d7f2a229bf04ab024d5d09f9820b96
Francis Gyimah <fgyimah883@gmail.com>
2023-11-07 22:07:37 +0000

    [DEV-11851] Updated StoryPublicationDate to support AlgoliaStory

--- packages/ui/src/components/StoryPublicationDate/StoryPublicationDate.tsx
+++ packages/ui/src/components/StoryPublicationDate/StoryPublicationDate.tsx
@@ -1,16 +1,20 @@
 import type { Culture, Story } from '@prezly/sdk';
-import { getStoryPublicationDate } from '@prezly/theme-kit-core';
+import { type AlgoliaStory, getStoryPublicationDate } from '@prezly/theme-kit-core';
 import { format } from 'date-fns';
 import * as locales from 'date-fns/locale';
 
 interface Props {
-    story: Story;
+    story: Story | AlgoliaStory;
     locale?: Culture['code'];
 }
 
 export function StoryPublicationDate({ story, locale: currentLocale }: Props) {
     const date = getStoryPublicationDate(story);
-    const dateFormat = story.newsroom.date_format;
+    let dateFormat = 'D/M/YY';
+
+    if ((story as Story).newsroom?.date_format) {
+        dateFormat = (story as Story).newsroom.date_format;
+    }
 
     if (!date) {
         return null;
