e6eadda31ac012c75d4941a62239f0c23e1f6280
Francis Gyimah <fgyimah883@gmail.com>
2023-11-07 11:44:12 +0000

    [DEV-11853] Added more stories for Footer component

--- packages/ui/src/components/Footer/Footer.stories.tsx
+++ packages/ui/src/components/Footer/Footer.stories.tsx
@@ -41,3 +41,48 @@ Default.args = {
     },
     locale: 'en',
 };
+
+export const WithoutExternalLink = FooterTemplate.bind({});
+WithoutExternalLink.args = {
+    companyInformation: COMPANY_INFORMATION,
+    hasStandaloneAboutPage: true,
+    hasStandaloneContactsPage: true,
+    publicGalleriesCount: 1,
+    newsroom: {
+        uuid: '13b5794c-619e-4c6f-961c-0b8e15d771c9',
+        custom_data_request_link: 'https://test.site',
+        display_name: 'Test newsroom',
+        is_white_labeled: false,
+    },
+    locale: 'en',
+};
+
+export const WithoutMediaAndStandalonePages = FooterTemplate.bind({});
+WithoutMediaAndStandalonePages.args = {
+    companyInformation: COMPANY_INFORMATION,
+    hasStandaloneAboutPage: false,
+    hasStandaloneContactsPage: false,
+    publicGalleriesCount: 0,
+    newsroom: {
+        uuid: '13b5794c-619e-4c6f-961c-0b8e15d771c9',
+        custom_data_request_link: 'https://test.site',
+        display_name: 'Test newsroom',
+        is_white_labeled: false,
+    },
+    locale: 'en',
+};
+
+export const IsWhiteLabeled = FooterTemplate.bind({});
+IsWhiteLabeled.args = {
+    companyInformation: COMPANY_INFORMATION,
+    hasStandaloneAboutPage: false,
+    hasStandaloneContactsPage: false,
+    publicGalleriesCount: 0,
+    newsroom: {
+        uuid: '13b5794c-619e-4c6f-961c-0b8e15d771c9',
+        custom_data_request_link: 'https://test.site',
+        display_name: 'Test newsroom',
+        is_white_labeled: true,
+    },
+    locale: 'en',
+};
