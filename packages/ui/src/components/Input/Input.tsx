0988af5de113896c6018c73e645fc6fb42a5ba19
Francis Gyimah <fgyimah883@gmail.com>
2023-11-07 22:02:56 +0000

    [DEV-11851] Updated Input component design

--- packages/ui/src/components/Input/Input.tsx
+++ packages/ui/src/components/Input/Input.tsx
@@ -1,23 +1,36 @@
-import type { InputHTMLAttributes } from 'react';
+import type { FunctionComponent, InputHTMLAttributes, SVGProps } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export interface Props extends InputHTMLAttributes<HTMLInputElement> {
     error?: string;
     inputClassName?: string;
+    icon?: FunctionComponent<Omit<SVGProps<SVGElement>, 'ref'>>;
+    iconPlacement?: 'left' | 'right';
 }
 
-export function Input({ className, inputClassName, error, ...inputProps }: Props) {
+export function Input({
+    className,
+    inputClassName,
+    error,
+    icon: Icon,
+    iconPlacement = 'left',
+    ...inputProps
+}: Props) {
     return (
         <div className={twMerge(`flex flex-col w-max`, className)}>
-            <input
-                {...inputProps}
-                className={twMerge(
-                    `flex rounded bg-white items-center justify-center w-full p-4 
+            <div className="flex items-center">
+                {Icon && iconPlacement === 'left' && <Icon className="w-5 h-5 mr-2" />}
+                <input
+                    {...inputProps}
+                    className={twMerge(
+                        `flex rounded bg-white items-center justify-center w-full p-4 
                     focus:outline-accent-lighter focus-within:outline-accent-lighter label-large`,
-                    error && `border border-error`,
-                    inputClassName,
-                )}
-            />
+                        error && `border border-error`,
+                        inputClassName,
+                    )}
+                />
+                {Icon && iconPlacement === 'right' && <Icon className="w-5 h-5 ml-2" />}
+            </div>
             {error && <span className="text-error label-medium mt-2">{error}</span>}
         </div>
     );
