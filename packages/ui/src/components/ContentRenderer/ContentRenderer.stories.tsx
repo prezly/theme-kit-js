2a790f145bd7f56610a11f92cd4e22748f7f4962
Francis Gyimah <fgyimah883@gmail.com>
2023-11-09 14:09:02 +0000

    [DEV-11948] Removed redundant eslint-ignore check

--- packages/ui/src/components/ContentRenderer/ContentRenderer.stories.tsx
+++ packages/ui/src/components/ContentRenderer/ContentRenderer.stories.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
 import { AnalyticsContextProvider } from '@prezly/analytics-nextjs';
 import type { Meta, StoryFn } from '@storybook/react';
 
