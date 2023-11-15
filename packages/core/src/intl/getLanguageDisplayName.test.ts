1a05ebd2d3aa759a7d1a24a0c3ea93e023e2eac2
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-15 14:35:07 +0200

    [DEV-11957] Extend `getLanguageDisplayName()` to work with locales too

--- packages/core/src/intl/getLanguageDisplayName.test.ts
+++ packages/core/src/intl/getLanguageDisplayName.test.ts
@@ -1,39 +1,69 @@
-import type { CultureRef } from '@prezly/sdk';
-import { Culture } from '@prezly/sdk';
+import type { Culture, CultureRef } from '@prezly/sdk';
 
 import { LANGUAGES } from '../__mocks__/languages';
 
 import { getLanguageDisplayName } from './getLanguageDisplayName';
 
 describe('getLanguageDisplayName', () => {
-    it('should return shortened name when language is the only culture', () => {
-        const languages = [LANGUAGES.en, LANGUAGES.fr];
+    describe('with languages as input', () => {
+        it('should return shortened name when language is the only culture', () => {
+            const languages = [LANGUAGES.en, LANGUAGES.fr];
 
-        expect(getLanguageDisplayName(LANGUAGES.en, languages)).toBe('English');
-        expect(getLanguageDisplayName(LANGUAGES.fr, languages)).toBe('Français');
+            expect(getLanguageDisplayName(LANGUAGES.en, languages)).toBe('English');
+            expect(getLanguageDisplayName(LANGUAGES.fr, languages)).toBe('Français');
+        });
+
+        it('should return full name when language is NOT the only culture', () => {
+            const languages = [LANGUAGES.nl_BE, LANGUAGES.nl_NL];
+
+            expect(getLanguageDisplayName(LANGUAGES.nl_BE, languages)).toBe('Nederlands (België)');
+            expect(getLanguageDisplayName(LANGUAGES.nl_NL, languages)).toBe(
+                'Nederlands (Nederland)',
+            );
+        });
+
+        it('should properly cut region from an RTL language name', () => {
+            const languages = [
+                {
+                    locale: {
+                        code: 'ar_AE',
+                        native_name: 'العربية (الإمارات العربية المتحدة)',
+                    } satisfies Pick<CultureRef, 'code' | 'native_name'>,
+                },
+            ];
+
+            expect(getLanguageDisplayName(languages[0], languages)).toBe('العربية');
+        });
     });
 
-    it('should return full name when language is NOT the only culture', () => {
-        const languages = [LANGUAGES.nl_BE, LANGUAGES.nl_NL];
+    describe('with locales as input', () => {
+        it('should return shortened name when language is the only culture', () => {
+            const locales = [LANGUAGES.en.locale, LANGUAGES.fr.locale];
 
-        expect(getLanguageDisplayName(LANGUAGES.nl_BE, languages)).toBe('Nederlands (België)');
-        expect(getLanguageDisplayName(LANGUAGES.nl_NL, languages)).toBe('Nederlands (Nederland)');
-    });
+            expect(getLanguageDisplayName(LANGUAGES.en.locale, locales)).toBe('English');
+            expect(getLanguageDisplayName(LANGUAGES.fr.locale, locales)).toBe('Français');
+        });
 
-    it('should properly cut region from an RTL language name', () => {
-        const languages = [
-            {
-                locale: {
+        it('should return full name when language is NOT the only culture', () => {
+            const locales = [LANGUAGES.nl_BE.locale, LANGUAGES.nl_NL.locale];
+
+            expect(getLanguageDisplayName(LANGUAGES.nl_BE.locale, locales)).toBe(
+                'Nederlands (België)',
+            );
+            expect(getLanguageDisplayName(LANGUAGES.nl_NL.locale, locales)).toBe(
+                'Nederlands (Nederland)',
+            );
+        });
+
+        it('should properly cut region from an RTL language name', () => {
+            const locales = [
+                {
                     code: 'ar_AE',
-                    locale: 'ar_AE',
-                    language_code: 'ar',
-                    name: 'Arabic (UAE)',
                     native_name: 'العربية (الإمارات العربية المتحدة)',
-                    direction: Culture.TextDirection.RTL,
-                } satisfies CultureRef,
-            },
-        ];
+                } satisfies Pick<Culture, 'code' | 'native_name'>,
+            ];
 
-        expect(getLanguageDisplayName(languages[0], languages)).toBe('العربية');
+            expect(getLanguageDisplayName(locales[0], locales)).toBe('العربية');
+        });
     });
 });
--- packages/core/src/intl/getLanguageDisplayName.ts
+++ packages/core/src/intl/getLanguageDisplayName.ts
@@ -1,5 +1,8 @@
-import type { NewsroomLanguageSettings } from '@prezly/sdk';
+import type { Culture, NewsroomLanguageSettings } from '@prezly/sdk';
 import { Locale } from '@prezly/theme-kit-intl';
+
+type MinCulture = Pick<Culture, 'code' | 'native_name'>;
+type MinLanguage = { locale: Pick<NewsroomLanguageSettings['locale'], 'code' | 'native_name'> };
 
 /**
  * @returns the display name of the locale in its native language
@@ -13,17 +16,31 @@ import { Locale } from '@prezly/theme-kit-intl';
  *  - English (Global), English (UK), Spanish (Spain)
  *  - -> English (Global), English (UK), Spanish
  */
-export function getLanguageDisplayName(
-    language: Pick<NewsroomLanguageSettings, 'locale'>,
-    languages: Pick<NewsroomLanguageSettings, 'locale'>[],
-): string {
-    const localeCode = language.locale.code;
+export function getLanguageDisplayName(subject: MinCulture, context: MinCulture[]): string;
 
-    const candidates = languages.filter((lang) => Locale.isSameLang(lang.locale.code, localeCode));
+export function getLanguageDisplayName(subject: MinLanguage, context: MinLanguage[]): string;
+
+export function getLanguageDisplayName<T extends MinLanguage | MinCulture>(
+    subject: T,
+    context: T[],
+): string {
+    const locale = toLocale(subject);
+    const locales = context.map(toLocale);
+
+    const candidates = locales.filter((candidate) =>
+        Locale.isSameLang(candidate.code, locale.code),
+    );
 
     if (candidates.length === 1) {
-        return language.locale.native_name.replace(/\s*\(.*/, '');
+        return locale.native_name.replace(/\s*\(.*/, '');
     }
 
-    return language.locale.native_name;
+    return locale.native_name;
+}
+
+function toLocale(value: MinCulture | MinLanguage): MinCulture {
+    if ('native_name' in value && 'code' in value) {
+        return value;
+    }
+    return value.locale;
 }
