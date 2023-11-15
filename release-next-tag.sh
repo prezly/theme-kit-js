409847d5db2c221ee8f3a6bcdc9404f03ea9f34a
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-15 22:48:55 +0200

    Write a script to release a `next` tag version

---
+++ release-next-tag.sh
@@ -0,0 +1,23 @@
+#/usr/bin/bash
+
+COMMIT=`git log | head -n1 | cut -d\  -f2`
+VERSION=0.0.0-next-$COMMIT
+
+echo Preparing $VERSION
+
+npx lerna version $VERSION --no-push --no-git-tag-version --yes
+
+npm run sync-package-versions
+
+npm install
+
+git add lerna.json \
+        package-lock.json \
+        packages/core/package.json \
+        packages/intl/package.json \
+        packages/nextjs/package.json \
+        packages/ui/package.json
+
+git commit -m "v$VERSION"
+
+npx lerna publish --dist-tag next from-package
