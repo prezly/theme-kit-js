495843bbeefc15690b3b358e136ad5d93d8f4647
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-15 15:19:50 +0200

    [DEV-11957] Cleanup search-related code

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,9 @@
 export * as Boilerplate from './boilerplate';
-export * as Categories from './categories';
 export * from './constants';
 export * as Galleries from './galleries';
 export * as Intl from './intl';
 export * as Metadata from './metadata';
 export * as Newsrooms from './newsrooms';
 export * as PrivacyPortal from './privacy-portal';
-export * from './types';
+export * as Search from './search';
 export * as Uploads from './uploads';
