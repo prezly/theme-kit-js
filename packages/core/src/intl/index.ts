d742dc83aff1fd7609ee3d2eb88e79352dcc59eb
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-15 15:38:14 +0200

    [DEV-11957] Move supported locale detection to the `intl` package

--- packages/core/src/intl/index.ts
+++ packages/core/src/intl/index.ts
@@ -1,7 +1,6 @@
 export { getLanguageDisplayName } from './getLanguageDisplayName';
 export { isNumberCode } from './isNumberCode';
 export { default as localeConfig } from './localeConfig';
-export { DEFAULT_LOCALE, getSupportedLocaleIsoCode } from './locale';
 
 // shortest locale slug generation and matching
 export { getShortestLocaleSlug } from './getShortestLocaleSlug';
