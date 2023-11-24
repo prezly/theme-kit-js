14640cb01bf0dfc651fceec2016da791fea7e21f
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-24 04:09:30 +0200

    [DEV-11989] Connect `generateUrl` to the Metadata adapter

--- packages/nextjs/src/adapters/metadata/server.ts
+++ packages/nextjs/src/adapters/metadata/server.ts
@@ -1,61 +1,65 @@
 import type { Metadata } from 'next';
 
 import * as lib from './lib';
-import type { Prerequisites } from './lib';
-
-type WithoutPrerequisites<T> = Omit<T, keyof Prerequisites>;
+import type { AppUrlGenerator, Prerequisites } from './lib';
 
 export namespace MetadataAdapter {
-    export type Configuration = Prerequisites;
+    export type Configuration = Prerequisites & {
+        generateUrl: AppUrlGenerator;
+    };
 
-    export function connect({ ...prerequisites }: Configuration) {
+    type WithoutPrerequisites<T> = Omit<T, keyof Prerequisites>;
+    type WithoutSharedConfiguration<T> = Omit<T, keyof Configuration>;
+
+    export function connect(configuration: Configuration) {
         function generatePageMetadata(
             params: WithoutPrerequisites<lib.generatePageMetadata.Parameters> = {},
             ...metadata: Metadata[]
         ) {
+            const { generateUrl, ...prerequisites } = configuration;
             return lib.generatePageMetadata({ ...prerequisites, ...params }, ...metadata);
         }
 
         function generateRootMetadata(
-            params: WithoutPrerequisites<lib.generateRootMetadata.Parameters> = {},
+            params: WithoutSharedConfiguration<lib.generateRootMetadata.Parameters> = {},
             ...metadata: Metadata[]
         ) {
-            return lib.generateRootMetadata({ ...prerequisites, ...params }, ...metadata);
+            return lib.generateRootMetadata({ ...configuration, ...params }, ...metadata);
         }
 
         function generateStoryPageMetadata(
-            params: WithoutPrerequisites<lib.generateStoryPageMetadata.Parameters>,
+            params: WithoutSharedConfiguration<lib.generateStoryPageMetadata.Parameters>,
             ...metadata: Metadata[]
         ) {
-            return lib.generateStoryPageMetadata({ ...prerequisites, ...params }, ...metadata);
+            return lib.generateStoryPageMetadata({ ...configuration, ...params }, ...metadata);
         }
 
         function generateCategoryPageMetadata(
-            params: WithoutPrerequisites<lib.generateCategoryPageMetadata.Parameters>,
+            params: WithoutSharedConfiguration<lib.generateCategoryPageMetadata.Parameters>,
             ...metadata: Metadata[]
         ) {
-            return lib.generateCategoryPageMetadata({ ...prerequisites, ...params }, ...metadata);
+            return lib.generateCategoryPageMetadata({ ...configuration, ...params }, ...metadata);
         }
 
         function generateMediaPageMetadata(
-            params: WithoutPrerequisites<lib.generateMediaPageMetadata.Parameters>,
+            params: WithoutSharedConfiguration<lib.generateMediaPageMetadata.Parameters>,
             ...metadata: Metadata[]
         ) {
-            return lib.generateMediaPageMetadata({ ...prerequisites, ...params }, ...metadata);
+            return lib.generateMediaPageMetadata({ ...configuration, ...params }, ...metadata);
         }
 
         function generateMediaAlbumPageMetadata(
-            params: WithoutPrerequisites<lib.generateMediaAlbumPageMetadata.Parameters>,
+            params: WithoutSharedConfiguration<lib.generateMediaAlbumPageMetadata.Parameters>,
             ...metadata: Metadata[]
         ) {
-            return lib.generateMediaAlbumPageMetadata({ ...prerequisites, ...params }, ...metadata);
+            return lib.generateMediaAlbumPageMetadata({ ...configuration, ...params }, ...metadata);
         }
 
         function generateSearchPageMetadata(
-            params: WithoutPrerequisites<lib.generateSearchPageMetadata.Parameters>,
+            params: WithoutSharedConfiguration<lib.generateSearchPageMetadata.Parameters>,
             ...metadata: Metadata[]
         ) {
-            return lib.generateSearchPageMetadata({ ...prerequisites, ...params }, ...metadata);
+            return lib.generateSearchPageMetadata({ ...configuration, ...params }, ...metadata);
         }
 
         return {
