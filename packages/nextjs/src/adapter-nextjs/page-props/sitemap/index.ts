97df278ab819b4d3cc6f9ffb2b1aa2ad04c2e1a1
Oleg Semyonov <oleg@prezly.com>
2023-12-12 18:18:00 +0400

    Clean up removed export

--- packages/nextjs/src/adapter-nextjs/page-props/sitemap/index.ts
+++ packages/nextjs/src/adapter-nextjs/page-props/sitemap/index.ts
@@ -1,5 +1,4 @@
 export { getSitemapServerSideProps } from './getServerSideProps';
 
 export { SitemapBuilder } from './SitemapBuilder';
-export { createPaths } from './createPaths';
 export * from './types';
