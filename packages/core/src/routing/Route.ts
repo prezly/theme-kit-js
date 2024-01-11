3809510d31f6f1dec727f1f8c94b93f506b51204
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-01-11 13:07:24 +0200

    [CARE-3777] Add `resolveLocale` to the Route interface

--- packages/core/src/routing/Route.ts
+++ packages/core/src/routing/Route.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-redeclare */
+import type { Locale } from '@prezly/theme-kit-intl';
 import UrlPattern from 'url-pattern';
 
 import type { ExtractPathParams } from './types';
@@ -25,12 +26,23 @@ export type Route<Pattern = string, Match = unknown> = {
      * Rewrite external route URL to the internal unambiguous pages layout URL.
      */
     rewrite(params: Match): string;
+
+    /**
+     * Resolve locale code from the request parameters.
+     *
+     * This WILL NOT be invoked automatically by the app during request routing
+     * (as we're super limited with what we can do in the middleware edge runtime).
+     * This method is expected to be invoked by the page & layout rendering components
+     * after the request has been routed.
+     */
+    resolveLocale(params: Match): Locale.Code | undefined;
 };
 
 export namespace Route {
     export interface Options<Pattern extends string, Match> {
         check?(match: Match, searchParams: URLSearchParams): boolean;
         generate?(pattern: UrlPattern, params: ExtractPathParams<Pattern>): `/${string}`;
+        resolveLocale?(params: ExtractPathParams<Pattern>): Locale.Code;
     }
 
     export function create<
@@ -39,10 +51,17 @@ export namespace Route {
     >(
         pattern: Pattern,
         rewrite: string,
-        { check, generate }: Options<Pattern, Match> = {},
+        { check, generate, resolveLocale }: Options<Pattern, Match> = {},
     ): Route<Pattern, Match> {
         const urlPattern = new UrlPattern(pattern);
         const rewritePattern = new UrlPattern(rewrite);
+
+        function defaultResolveLocale(params: Match) {
+            if ('localeCode' in params) {
+                return params.localeCode as Locale.Code;
+            }
+            return undefined;
+        }
 
         return {
             pattern,
@@ -68,6 +87,9 @@ export namespace Route {
             },
             rewrite(params: Match) {
                 return rewritePattern.stringify(params);
+            },
+            resolveLocale(params: Match): Locale.Code | undefined {
+                return (resolveLocale ?? defaultResolveLocale)(params);
             },
         };
     }
