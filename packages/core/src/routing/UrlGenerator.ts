cd956abbe0755992a1de2cded39ea3939418b5f1
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-01-16 13:27:59 +0200

    Fix lint complaints

--- packages/core/src/routing/UrlGenerator.ts
+++ packages/core/src/routing/UrlGenerator.ts
@@ -2,7 +2,9 @@
 import type { Route } from './Route';
 import type { Router } from './Router';
 
-type WithLocaleCode<T> = T extends { localeSlug: string }
+type WithLocaleCode<T> = T extends {
+    localeSlug: string;
+}
     ? Omit<T, 'localeSlug'> & { localeCode: string }
     : T extends { localeSlug?: string }
       ? Omit<T, 'localeSlug'> & { localeCode?: string }
@@ -13,16 +15,17 @@ type WithLocaleCode<T> = T extends { localeSlug: string }
  * - required `localeSlug` can be replaced with required `localeCode`
  * - optional `localeSlug` can be replaced with optional `localeCode`
  */
-export type UrlGenerator<T> = T extends Router<infer Routes>
-    ? <RouteName extends keyof Routes>(
-          routeName: RouteName,
-          ...params: Routes[RouteName] extends Route<string, infer Match>
-              ? {} extends Match
-                  ? [WithLocaleCode<Match>] | [Match] | []
-                  : [WithLocaleCode<Match>] | [Match]
-              : never
-      ) => `/${string}`
-    : never;
+export type UrlGenerator<T> =
+    T extends Router<infer Routes>
+        ? <RouteName extends keyof Routes>(
+              routeName: RouteName,
+              ...params: Routes[RouteName] extends Route<string, infer Match>
+                  ? {} extends Match
+                      ? [WithLocaleCode<Match>] | [Match] | []
+                      : [WithLocaleCode<Match>] | [Match]
+                  : never
+          ) => `/${string}`
+        : never;
 
 export namespace UrlGenerator {
     /**
@@ -34,24 +37,32 @@ export namespace UrlGenerator {
      * This is the same as UrlGenerator<T>, but for absolute URLs.
      * @see UrlGenerator.
      */
-    export type Absolute<T> = T extends Router<infer Routes>
-        ? <RouteName extends keyof Routes>(
-              routeName: RouteName,
-              ...params: Routes[RouteName] extends Route<string, infer Match>
-                  ? {} extends Match
-                      ? [WithLocaleCode<Match>] | [Match] | []
-                      : [WithLocaleCode<Match>] | [Match]
-                  : never
-          ) => `http://${string}` | `https://${string}`
-        : never;
+    export type Absolute<T> =
+        T extends Router<infer Routes>
+            ? <RouteName extends keyof Routes>(
+                  routeName: RouteName,
+                  ...params: Routes[RouteName] extends Route<string, infer Match>
+                      ? {} extends Match
+                          ? [WithLocaleCode<Match>] | [Match] | []
+                          : [WithLocaleCode<Match>] | [Match]
+                      : never
+              ) => `http://${string}` | `https://${string}`
+            : never;
 
-    export type Params<T> = T extends Router<infer Routes>
-        ? {
-              [RouteName in keyof Routes]: Routes[RouteName] extends Route<string, infer Match>
-                  ? {} extends Match
-                      ? { routeName: RouteName; params?: WithLocaleCode<Match> | Match }
-                      : { routeName: RouteName; params: WithLocaleCode<Match> | Match }
-                  : never;
-          }[keyof Routes]
-        : never;
+    export type Params<T> =
+        T extends Router<infer Routes>
+            ? {
+                  [RouteName in keyof Routes]: Routes[RouteName] extends Route<string, infer Match>
+                      ? {} extends Match
+                          ? {
+                                routeName: RouteName;
+                                params?: WithLocaleCode<Match> | Match;
+                            }
+                          : {
+                                routeName: RouteName;
+                                params: WithLocaleCode<Match> | Match;
+                            }
+                      : never;
+              }[keyof Routes]
+            : never;
 }
