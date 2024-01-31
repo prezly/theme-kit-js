1a147e3d6d71723b1ca1e286e2188366a7892e23
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-01-31 02:34:47 +0200

    [DEV-12376] Extract duplicate Button & ButtonLink theme into a shared object

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -1,56 +1,8 @@
-import type { ButtonHTMLAttributes } from 'react';
+import type { ButtonHTMLAttributes, Ref } from 'react';
 
 import { Icon } from './Icon';
-import { createStyling } from './styling';
-import type { Theme as GenericTheme } from './theming';
+import { type Theme as ButtonTheme, theme as defaultTheme } from './theme';
 import { type BaseProps, ButtonSize, ButtonVariant } from './types';
-
-const defaultTheme: Button.Theme = {
-    button: createStyling<BaseProps>(
-        'flex items-center justify-center gap-2',
-        'border border-transparent bg-transparent',
-        // 'focus:ring-4 focus:ring-accent-lighter',
-        'focus-visible:ring-4 focus-visible:ring-accent-lighter',
-        'leading-[126%]',
-        {
-            rounded: {
-                $off: 'rounded',
-                $on: 'rounded-full',
-            },
-            size: {
-                default: 'p-3 label-large',
-                small: 'p-2 label-medium',
-            },
-            variation: {
-                primary: [
-                    'bg-accent border-accent text-accent-button-text',
-                    'hover:bg-accent-dark hover:border-accent-dark',
-                    // 'focus:bg-accent-dark focus:border-transparent',
-                    'focus-visible:bg-accent-dark focus-visible:border-transparent',
-                    'active:bg-accent-dark active:border-accent-dark',
-                    'disabled:bg-accent-lighter disabled:border-transparent disabled:opacity-50',
-                ],
-                secondary: [
-                    'bg-white border-gray-200 text-gray-800',
-                    'hover:bg-gray-50 hover:border-gray-300',
-                    // 'focus:bg-gray-50 focus:border-transparent',
-                    'focus-visible:bg-gray-50 focus-visible:border-transparent',
-                    'active:bg-gray-100 active:border-gray-400',
-                    'disabled:bg-white disabled:border-transparent disabled:text-gray-400',
-                ],
-                navigation: [
-                    'p-0 bg-white text-gray-800',
-                    'hover:text-gray-900',
-                    // 'focus:text-gray-950',
-                    'focus-visible:text-gray-950',
-                    'disabled:bg-white disabled:text-gray-400',
-                ],
-            },
-        },
-    ),
-    content: createStyling<BaseProps>('first:ml-1 last:mr-1 empty:hidden'),
-    icon: () => '',
-};
 
 export function Button(props: Button.Props) {
     const {
@@ -79,7 +31,9 @@ export function Button(props: Button.Props) {
             {...attributes}
         >
             {iconPlacement === 'left' && <Icon icon={icon} className={theme.icon(compiledProps)} />}
+
             <span className={theme.content(compiledProps)}>{children}</span>
+
             {iconPlacement === 'right' && (
                 <Icon icon={icon} className={theme.icon(compiledProps)} />
             )}
@@ -90,7 +44,8 @@ export function Button(props: Button.Props) {
 export namespace Button {
     export type Props = BaseProps &
         ButtonHTMLAttributes<HTMLButtonElement> & {
-            theme: Theme;
+            theme?: Theme;
+            forwardRef?: Ref<HTMLButtonElement>;
         };
 
     export type Variant = ButtonVariant;
@@ -99,6 +54,6 @@ export namespace Button {
     export type Size = ButtonSize;
     export const Size = ButtonSize;
 
-    export type Theme = GenericTheme<'button' | 'content' | 'icon', BaseProps>;
+    export type Theme = ButtonTheme;
     export const theme: Theme = defaultTheme;
 }
---
+++ packages/ui/src/components/Button/theme.ts
@@ -0,0 +1,52 @@
+import { createStyling } from './styling';
+import type { Theme as GenericTheme } from './theming';
+import type { BaseProps } from './types';
+
+export type Theme = GenericTheme<'button' | 'content' | 'icon', BaseProps>;
+
+export const theme: Theme = {
+    button: createStyling<BaseProps>(
+        'flex items-center justify-center gap-2',
+        'border border-transparent bg-transparent',
+        // 'focus:ring-4 focus:ring-accent-lighter',
+        'focus-visible:ring-4 focus-visible:ring-accent-lighter',
+        'leading-[126%]',
+        {
+            rounded: {
+                $off: 'rounded',
+                $on: 'rounded-full',
+            },
+            size: {
+                default: 'p-3 label-large',
+                small: 'p-2 label-medium',
+            },
+            variation: {
+                primary: [
+                    'bg-accent border-accent text-accent-button-text',
+                    'hover:bg-accent-dark hover:border-accent-dark',
+                    // 'focus:bg-accent-dark focus:border-transparent',
+                    'focus-visible:bg-accent-dark focus-visible:border-transparent',
+                    'active:bg-accent-dark active:border-accent-dark',
+                    'disabled:bg-accent-lighter disabled:border-transparent disabled:opacity-50',
+                ],
+                secondary: [
+                    'bg-white border-gray-200 text-gray-800',
+                    'hover:bg-gray-50 hover:border-gray-300',
+                    // 'focus:bg-gray-50 focus:border-transparent',
+                    'focus-visible:bg-gray-50 focus-visible:border-transparent',
+                    'active:bg-gray-100 active:border-gray-400',
+                    'disabled:bg-white disabled:border-transparent disabled:text-gray-400',
+                ],
+                navigation: [
+                    'p-0 bg-white text-gray-800',
+                    'hover:text-gray-900',
+                    // 'focus:text-gray-950',
+                    'focus-visible:text-gray-950',
+                    'disabled:bg-white disabled:text-gray-400',
+                ],
+            },
+        },
+    ),
+    content: createStyling<BaseProps>('first:ml-1 last:mr-1 empty:hidden'),
+    icon: () => '',
+};
