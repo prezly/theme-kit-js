5140cf77132dfe6f34efbedcf3b94cb962cb5c70
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-02-07 01:05:50 +0200

    [DEV-12376] Fix tailwind CSS build

--- packages/ui/postcss.config.js
+++ packages/ui/postcss.config.js
@@ -1,6 +1,31 @@
+const path = require('path');
+
 module.exports = {
     plugins: {
-        'postcss-import': {},
+        'postcss-import': {
+            /**
+             * @typedef ImportOptions
+             * @property {string} root
+             * @property {string[]} path
+             * @property {boolean} skipDuplicates
+             * @property {Function} resolve
+             * @property {Function} load
+             * @property {Array} plugins
+             * @property {Array} addModulesDirectores
+             * @property {boolean} warnOnEmpty
+             *
+             * @param {string} id
+             * @param {string} basedir
+             * @param {ImportOptions} importOptions
+             */
+            resolve(id, basedir, { resolve }) {
+                const p = id.endsWith('.css') ? id : `${id}.css`;
+                if (p.startsWith('.')) {
+                    return path.resolve(basedir, p);
+                }
+                return require.resolve(p);
+            },
+        },
         tailwindcss: {},
         autoprefixer: {},
     },
