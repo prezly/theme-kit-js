f7a502fc3fa47ef6c90c7571d0700bd08e0d1d51
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-02-23 16:59:08 +0100

    [DEV-12530] Implement support for pluralization tokens

--- packages/intl/src/formatting/messages/lib.test.ts
+++ packages/intl/src/formatting/messages/lib.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import { formatMessageFragment, formatMessageString } from './lib';
+import { formatMessageFragment, formatMessageString, parseMessage, tokenizeMessage } from './lib';
 import type { IntlDictionary } from './types';
 
 const I18N: IntlDictionary = {
@@ -15,6 +15,50 @@ const I18N: IntlDictionary = {
         {
             type: 0,
             value: '!',
+        },
+    ],
+    [`images.count`]: [
+        {
+            type: 0,
+            value: 'Images: ',
+        },
+        {
+            offset: 0,
+            options: {
+                '=0': {
+                    value: [
+                        {
+                            type: 0,
+                            value: 'no images',
+                        },
+                    ],
+                },
+                one: {
+                    value: [
+                        {
+                            type: 7,
+                        },
+                        {
+                            type: 0,
+                            value: ' image',
+                        },
+                    ],
+                },
+                other: {
+                    value: [
+                        {
+                            type: 7,
+                        },
+                        {
+                            type: 0,
+                            value: ' images',
+                        },
+                    ],
+                },
+            },
+            pluralType: 'cardinal',
+            type: 6,
+            value: 'imagesCount',
         },
     ],
 };
@@ -49,6 +93,37 @@ describe('formatMessageString', () => {
 
         expect(formatted).toBe('Hello, Julio!');
     });
+
+    it('should format pluralization placeholders', () => {
+        function format(imagesCount: number) {
+            return formatMessageString({ id: 'images.count', defaultMessage: 'default' }, I18N, {
+                imagesCount,
+            });
+        }
+
+        expect(format(0)).toBe('Images: no images');
+        expect(format(1)).toBe('Images: 1 images');
+        expect(format(1024)).toBe('Images: 1024 images');
+    });
+
+    it('should do the most minimal support for pluralization placeholders in default messages', () => {
+        function format(imagesCount: number) {
+            return formatMessageString(
+                {
+                    id: 'images.count',
+                    defaultMessage: 'Images: {imagesCount, plural, other {IDK}}',
+                },
+                {},
+                {
+                    imagesCount,
+                },
+            );
+        }
+
+        expect(format(0)).toEqual('Images: 0');
+        expect(format(1)).toEqual('Images: 1');
+        expect(format(1024)).toEqual('Images: 1024');
+    });
 });
 
 describe('formatMessageFragment', () => {
@@ -80,6 +155,87 @@ describe('formatMessageFragment', () => {
         );
 
         expect(formatted).toEqual(['Hello, ', { type: 'strong', children: ['Julio'] }, '!']);
+    });
+
+    it('should format pluralization placeholders', () => {
+        function format(imagesCount: number) {
+            return formatMessageFragment({ id: 'images.count', defaultMessage: 'default' }, I18N, {
+                imagesCount,
+            });
+        }
+
+        expect(format(0)).toEqual(['Images: ', 'no images']);
+        expect(format(1)).toEqual(['Images: ', '1', ' images']);
+        expect(format(1024)).toEqual(['Images: ', '1024', ' images']);
+    });
+
+    it('should do the most minimal support for pluralization placeholders in default messages', () => {
+        function format(imagesCount: number) {
+            return formatMessageFragment(
+                {
+                    id: 'images.count',
+                    defaultMessage: 'Images: {imagesCount, plural, other {IDK}}',
+                },
+                {},
+                {
+                    imagesCount,
+                },
+            );
+        }
+
+        expect(format(0)).toEqual(['Images: ', 0]);
+        expect(format(1)).toEqual(['Images: ', 1]);
+        expect(format(1024)).toEqual(['Images: ', 1024]);
+    });
+});
+
+describe('parseMessage', () => {
+    it('should parse literal string', () => {
+        const parsed = parseMessage('Hello world!');
+        expect(parsed).toEqual([{ type: 0, value: 'Hello world!' }]);
+    });
+
+    it('should parse argument placeholders', () => {
+        const parsed = parseMessage('Hello {name}!');
+        expect(parsed).toEqual([
+            { type: 0, value: 'Hello ' },
+            { type: 1, value: 'name' },
+            { type: 0, value: '!' },
+        ]);
+    });
+
+    it('should partially parse plural placeholders as a last resort fallback', () => {
+        const parsed = parseMessage(
+            '{apples, plural, zero {There are no apples}, one {There is one apple}, other {There are # apples}}',
+        );
+        expect(parsed).toEqual([{ type: 1, value: 'apples' }]);
+    });
+});
+
+describe('tokenizeMessage', () => {
+    it('should return a single item array for literal strings', () => {
+        const tokens = tokenizeMessage('Hello world');
+        expect(tokens).toEqual(['Hello world']);
+    });
+
+    it('it should tokenize simple placeholders', () => {
+        const tokens = tokenizeMessage('Hello {type} world of {creatures}!');
+        expect(tokens).toEqual(['Hello ', '{type}', ' world of ', '{creatures}', '!']);
+    });
+
+    it('it should tokenize pluralization placeholders', () => {
+        const tokens = tokenizeMessage(
+            'Hello {type} world of {count, plural, other {#}} {creatures}!',
+        );
+        expect(tokens).toEqual([
+            'Hello ',
+            '{type}',
+            ' world of ',
+            '{count, plural, other {#}}',
+            ' ',
+            '{creatures}',
+            '!',
+        ]);
     });
 });
 
--- packages/intl/src/formatting/messages/types.ts
+++ packages/intl/src/formatting/messages/types.ts
@@ -1,54 +1,55 @@
 export type IntlDictionary = Partial<Record<string, IntlMessageFormat>>;
 
-/**
- * @see https://github.com/formatjs/formatjs/blob/df4f21270320237eb5a91fbf74f3f8f51b4ff184/rust/icu-messageformat-parser/src/ast.rs#L212-L262
- */
-enum Type {
-    /**
-     * Raw text
-     */
-    LITERAL = 0,
-    /**
-     * Variable without any format, e.g. `var` in `this is a {var}`
-     */
-    ARGUMENT = 1,
-    // NUMBER = 2, // Not implemented
-    // DATE = 3, // Not implemented
-    // TIME = 4, // Not implemented
-    // SELECT = 5, // Not implemented
-    /**
-     * Variable with plural format
-     */
-    PLURAL = 6,
-    /**
-     * Only possible within plural argument.
-     * This is the `#` symbol that will be substituted with the count.
-     */
-    POUND = 7,
-    // TAG = 8, // Not implemented
-}
-
 type VarName = string;
 
-type PluralOption = {
-    value: IntlMessageFormat;
-};
-
 export type IntlMessageFormat = (
-    | { type: Type.LITERAL; value: string }
-    | { type: Type.ARGUMENT; value: VarName }
+    | { type: IntlMessageFormat.Type.LITERAL; value: string }
+    | { type: IntlMessageFormat.Type.ARGUMENT; value: VarName }
     | {
-          type: Type.PLURAL;
+          type: IntlMessageFormat.Type.PLURAL;
           value: VarName;
           pluralType: 'cardinal' | 'ordinal';
           offset: number;
-          options: Record<
-              'zero' | 'one' | 'two' | 'few' | 'many' | 'other' | `=${number}`,
-              PluralOption
-          >;
+          options: Partial<
+              Record<
+                  'zero' | 'one' | 'two' | 'few' | 'many' | `=${number}`,
+                  { value: IntlMessageFormat }
+              >
+          > & { other: { value: IntlMessageFormat } };
       }
-    | { type: Type.POUND }
+    | { type: IntlMessageFormat.Type.POUND }
 )[];
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export namespace IntlMessageFormat {
+    /**
+     * @see https://github.com/formatjs/formatjs/blob/df4f21270320237eb5a91fbf74f3f8f51b4ff184/rust/icu-messageformat-parser/src/ast.rs#L212-L262
+     */
+    export enum Type {
+        /**
+         * Raw text
+         */
+        LITERAL = 0,
+        /**
+         * Variable without any format, e.g. `var` in `this is a {var}`
+         */
+        ARGUMENT = 1,
+        // NUMBER = 2, // Not implemented
+        // DATE = 3, // Not implemented
+        // TIME = 4, // Not implemented
+        // SELECT = 5, // Not implemented
+        /**
+         * Variable with plural format
+         */
+        PLURAL = 6,
+        /**
+         * Only possible within plural argument.
+         * This is the `#` symbol that will be substituted with the count.
+         */
+        POUND = 7,
+        // TAG = 8, // Not implemented
+    }
+}
 
 export interface IntlMessageDescriptor {
     id: string;
