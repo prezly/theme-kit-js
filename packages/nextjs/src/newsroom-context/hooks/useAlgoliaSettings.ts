f5f1942d5dcbb32772fa8af288cf9f10938a73f8
Yuriy Yakym <yakym.yuriy@gmail.com>
2024-07-16 10:06:28 +0200

    Replace `useAlgoliaSettings` with `useSearchSettings`

--- packages/nextjs/src/newsroom-context/hooks/useAlgoliaSettings.ts
+++
@@ -1,7 +0,0 @@
-import { useNewsroomContext } from './useNewsroomContext';
-
-export function useAlgoliaSettings() {
-    const context = useNewsroomContext();
-
-    return context.algoliaSettings;
-}
