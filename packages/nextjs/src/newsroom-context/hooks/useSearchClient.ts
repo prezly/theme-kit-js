bfe3320d170514d54557f97c2f493e1cb314fd3e
Yuriy Yakym <yakym.yuriy@gmail.com>
2024-07-16 16:13:04 +0200

    Update useSearchClient to accept settings as props

--- packages/nextjs/src/newsroom-context/hooks/useSearchClient.ts
+++ packages/nextjs/src/newsroom-context/hooks/useSearchClient.ts
@@ -1,16 +1,8 @@
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
+import type { SearchSettings } from '@prezly/theme-kit-core/server';
 import algoliasearch from 'algoliasearch';
 
-
-import { useSearchSettings } from './useSearchSettings';
-
-export function useSearchClient() {
-    const settings = useSearchSettings();
-
-    if (!settings) {
-        return null;
-    }
-
+export function useSearchClient(settings: SearchSettings) {
     return settings.searchBackend === 'algolia'
         ? algoliasearch(settings.appId, settings.apiKey)
         : instantMeiliSearch(settings.host, settings.apiKey).searchClient;
