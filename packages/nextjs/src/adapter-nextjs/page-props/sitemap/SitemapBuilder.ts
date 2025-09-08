cea2efe1ae4a4ca5124c90f913cf79030322c38a
kudlajz <kudlajzzz@gmail.com>
2025-09-08 17:13:24 +0200

    Remove extra namespaces from sitemap

--- packages/nextjs/src/adapter-nextjs/page-props/sitemap/SitemapBuilder.ts
+++ packages/nextjs/src/adapter-nextjs/page-props/sitemap/SitemapBuilder.ts
@@ -160,7 +160,7 @@ export class SitemapBuilder {
     serialize() {
         return [
             '<?xml version="1.0" encoding="UTF-8" ?>',
-            '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.w3.org/TR/xhtml11/xhtml11_schema.html http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/TR/xhtml11/xhtml11_schema.html">',
+            '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
             this.urls.map(SitemapBuilder.serializeLocation).join('\n'),
             '</urlset>',
         ].join('\n');
